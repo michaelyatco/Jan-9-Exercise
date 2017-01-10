1fdfc8dd214314bd6c5970c209ce262aa9072b93
Michael Yatco <mjyatco@gmail.com>
2017-01-09 21:08:02 -0500

    Now this upload has a class with a single option hash for the intialize method

--- store_item.rb
+++ store_item.rb
@@ -13,11 +13,17 @@ class Chocolate
   attr_reader :region, :price, :flavor
   attr_writer :price
 
-  def initialize(input_region, input_price, input_flavor)
-    @region = input_region
-    @price = input_price
-    @flavor = input_flavor
-  end
+  # def initialize(input_region, input_price, input_flavor)
+  #   @region = input_region
+  #   @price = input_price
+  #   @flavor = input_flavor
+  # end
+
+    def initialize(input_hash)
+      @region = input_hash[:region]
+      @price = input_hash[:price]
+      @flavor = input_hash[:flavor]
+    end
 
   def print_info
     puts "This chocolate hails from #{region} with a rich #{flavor} flavor and can be yours for the price of $#{price}. Bon appetit!"
@@ -25,9 +31,13 @@ class Chocolate
 
 end
 
-chocolate1 = Chocolate.new("France", 100, "dark")
-puts chocolate1
-puts chocolate1.print_info
-chocolate1.price = 1000
-puts chocolate1.price
-puts chocolate1.print_info
+# chocolate1 = Chocolate.new("France", 50, "dark")
+# puts chocolate1
+# puts chocolate1.print_info
+# chocolate1.price = 1000
+# puts chocolate1.price
+# puts chocolate1.print_info
+
+chocolate2 = Chocolate.new(flavor: "bittersweet", price: 100, region: "Switzerland")
+puts chocolate2
+puts chocolate2.print_info
