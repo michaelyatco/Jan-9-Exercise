92e70931f27727b4d522364be48b3c9332d58291
Michael Yatco <mjyatco@gmail.com>
2017-01-09 20:38:16 -0500

    Here is the store_items code as a class now

--- store_item.rb
+++ store_item.rb
@@ -1,9 +1,33 @@
-#JavaScript Syntax
-chocolate1 = {region: "France", price: 50, flavor: "dark"}
-chocolate2 = {region: "Switzerland", price: 100, flavor: "bittersweet"}
-chocolate3 = {region: "San Francisco", price: 25, flavor: "milk"}
+# #JavaScript Syntax
+# chocolate1 = {region: "France", price: 50, flavor: "dark"}
+# chocolate2 = {region: "Switzerland", price: 100, flavor: "bittersweet"}
+# chocolate3 = {region: "San Francisco", price: 25, flavor: "milk"}
 
-#Ruby Syntax
-chocolate1 = {:region => "France", :price => 50, :flavor => "dark"}
-chocolate2 = {:region => "Switzerland", :price => 100, :flavor => "bittersweet"}
-chocolate3 = {:region => "San Francisco", :price => 25, :flavor => "milk"}
+# #Ruby Syntax
+# chocolate1 = {:region => "France", :price => 50, :flavor => "dark"}
+# chocolate2 = {:region => "Switzerland", :price => 100, :flavor => "bittersweet"}
+# chocolate3 = {:region => "San Francisco", :price => 25, :flavor => "milk"}
+
+class Chocolate
+
+  attr_reader :region, :price, :flavor
+  attr_writer :price
+
+  def initialize(input_region, input_price, input_flavor)
+    @region = input_region
+    @price = input_price
+    @flavor = input_flavor
+  end
+
+  def print_info
+    puts "This chocolate hails from #{region} with a rich #{flavor} flavor and can be yours for the price of $#{price}. Bon appetit!"
+  end
+
+end
+
+chocolate1 = Chocolate.new("France", 100, "dark")
+puts chocolate1
+puts chocolate1.print_info
+chocolate1.price = 1000
+puts chocolate1.price
+puts chocolate1.print_info
