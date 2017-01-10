6201f60adb344d5d0523f26f3e85c76c962798f6
Michael Yatco <mjyatco@gmail.com>
2017-01-09 19:57:22 -0500

    Here is the store_items code

---
+++ store_item.rb
@@ -0,0 +1,9 @@
+#JavaScript Syntax
+chocolate1 = {region: "France", price: 50, flavor: "dark"}
+chocolate2 = {region: "Switzerland", price: 100, flavor: "bittersweet"}
+chocolate3 = {region: "San Francisco", price: 25, flavor: "milk"}
+
+#Ruby Syntax
+chocolate1 = {:region => "France", :price => 50, :flavor => "dark"}
+chocolate2 = {:region => "Switzerland", :price => 100, :flavor => "bittersweet"}
+chocolate3 = {:region => "San Francisco", :price => 25, :flavor => "milk"}
